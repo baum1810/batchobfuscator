426c81cf97e866346c8116d05e9ccb9faa5399a3
K.Dot <110929758+KDot227@users.noreply.github.com>
2022-09-22 19:43:56 -0500

    Update obfuscator.py

--- obfuscator.py
+++ obfuscator.py
@@ -11,41 +11,41 @@ def main():
         except:
             pass
         f = open(f"{file}obfusc.bat", "a")
-        f.write("::obfuscated by https://github.com/baum1810")
+        f.write("::obfuscated by https://github.com/baum1810\n")
         f.close()
         for line in fileobj:
            for ch in line:
                 if not counter == 1:
                     if "\n" in ch:
-                        f = open(f"{file}obfusc.bat", "a")
+                        f = open(f"{file}obfusc.bat", "a", encoding='utf-8')
                         f.write(f"\n")
                         f.close()
                     elif "%" in ch:
 
-                        f = open(f"{file}obfusc.bat", "a")
+                        f = open(f"{file}obfusc.bat", "a", encoding='utf-8')
                         f.write(f"%")
                         f.close()
                         counter = 1
                     else:
                          n = random.randint(1,10)
                          randomi = ''.join(random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') for _ in range(n))
-                         f = open(f"{file}obfusc.bat", "a")
+                         f = open(f"{file}obfusc.bat", "a", encoding='utf-8')
                          f.write(f"{ch}%{randomi}%")
                          f.close()
                 else:
 
 
                     if "%" in ch:
-                        f = open(f"{file}obfusc.bat", "a")
+                        f = open(f"{file}obfusc.bat", "a", encoding='utf-8')
                         f.write("%")
                         f.close()
                         counter = 0
                     elif "\n" in ch:
-                        f = open(f"{file}obfusc.bat", "a")
+                        f = open(f"{file}obfusc.bat", "a", encoding='utf-8')
                         f.write(f"\n")
                         f.close()
                     else:
-                        f = open(f"{file}obfusc.bat", "a")
+                        f = open(f"{file}obfusc.bat", "a", encoding='utf-8')
                         f.write(ch)
                         f.close()
     main()
